3ae8ac604e95e760b21d562c5da8b086a7827a8b
kylec43 <56184985+kylec43@users.noreply.github.com>
2021-11-03 17:27:07 -0500

    added login design. added new pages

--- functions/index.js
+++ functions/index.js
@@ -12,8 +12,41 @@ exports.httpReq = functions.https.onRequest(app);
 
 
 app.get('/', (req, res) => {
+    res.render(Pages.LOGIN_PAGE, {message: "This is a message"});
+});
 
-    //res.send("Hello World");
-    res.render(Pages.HOME_PAGE, {message: "This is a message"});
+
+app.get('/forgot_password', (req, res) => {
+    res.render(Pages.FORGOT_PASSWORD_PAGE, {message: "This is a message"});
+});
+
+
+app.get('/register', (req, res) => {
+    res.render(Pages.REGISTER_PAGE, {message: "This is a message"});
+});
+
+
+app.get('/compose', (req, res) => {
+    res.render(Pages.COMPOSE_PAGE, {message: "This is a message"});
+});
+
+
+app.get('/drafts', (req, res) => {
+    res.render(Pages.DRAFTS_PAGE, {message: "This is a message"});
+});
+
+
+app.get('/inbox', (req, res) => {
+    res.render(Pages.INBOX_PAGE, {message: "This is a message"});
+});
+
+
+app.get('/sent', (req, res) => {
+    res.render(Pages.SENT_PAGE, {message: "This is a message"});
+});
+
+
+app.get('/mediaplayer', (req, res) => {
+    res.render(Pages.MEDIAPLAYER_PAGE, {message: "This is a message"});
 });
 
--- functions/model/constants.js
+++ functions/model/constants.js
@@ -1,5 +1,12 @@
 pages = {
-    HOME_PAGE: "home_page.ejs",
+    INBOX_PAGE: "inbox_page.ejs",
+    SENT_PAGE: "sent_page.ejs",
+    LOGIN_PAGE: "login_page.ejs",
+    REGISTER_PAGE: "register_page.ejs",
+    DRAFTS_PAGE: "drafts_page.ejs",
+    FORGOT_PASSWORD_PAGE: "forgot_password_page.ejs",
+    COMPOSE_PAGE: "compose_page.ejs",
+    MEDIAPLAYER_PAGE: "mediaplayer_page.ejs",
 }
 
 
